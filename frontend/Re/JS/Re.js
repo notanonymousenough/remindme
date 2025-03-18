004881a6070a79c39d0d879b379f600b732305bd
Bivol Nikita <titanov.tor@mail.ru>
2025-03-18 17:23:55 +0300

    Фронт + логика ссылок

---
+++ frontend/Re/JS/Re.js
@@ -0,0 +1,23 @@
+function add_teg()
+{
+    let element = document.getElementById('add-teg');
+    element.style.display = 'block';
+    let btn_teg = document.getElementById('btn-add');
+    btn_teg.style.display = 'none';
+
+}
+function save_teg()
+{
+    let element = document.getElementById('add-teg');
+    element.style.display = 'none';
+    let btn_teg = document.getElementById('btn-add');
+    btn_teg.style.display = 'block';
+
+}
+function cancel_teg()
+{
+    let element = document.getElementById('add-teg');
+    element.style.display = 'none';
+    let btn_teg = document.getElementById('btn-add');
+    btn_teg.style.display = 'block';
+}
